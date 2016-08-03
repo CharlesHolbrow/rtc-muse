bbc16f9b231661a14b634f0a0b4e216f2a215160
Charles Holbrow <charlesholbrow@gmail.com>
2016-08-03 18:48:01 -0400

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -10,34 +10,35 @@ var dir_html = path.resolve(__dirname, 'html');
 var dir_build = path.resolve(__dirname, 'build');
 
 module.exports = {
-	entry: path.resolve(dir_js, 'main.js'),
-	output: {
-		path: dir_build,
-		filename: 'bundle.js'
-	},
-	devServer: {
-		contentBase: dir_build,
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				test: dir_js,
-			}
-		]
-	},
-	plugins: [
-		// Simply copies the files over
-		new CopyWebpackPlugin([
-			{ from: dir_html } // to: output.path
-		]),
-		// Avoid publishing files when compilation fails
-		new webpack.NoErrorsPlugin()
-	],
-	stats: {
-		// Nice colored output
-		colors: true
-	},
-	// Create Sourcemaps for the bundle
-	devtool: 'source-map',
+  entry: path.resolve(dir_js, 'main.js'),
+  output: {
+    path: dir_build,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: dir_build,
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: dir_js,
+        include: [dir_js],
+      }
+    ]
+  },
+  plugins: [
+    // Simply copies the files over
+    new CopyWebpackPlugin([
+      { from: dir_html } // to: output.path
+    ]),
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin()
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
